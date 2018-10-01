3150151fb6b0652d3c244bd0a48443d76762442f
Korhan <korhan.bircan@capitalone.com>
2018-10-01 15:42:17 -0700

    Added the database callback methods.

--- app/src/main/java/com/korhan34/roomwithaview/WordRoomDatabase.java
+++ app/src/main/java/com/korhan34/roomwithaview/WordRoomDatabase.java
@@ -1,13 +1,41 @@
 package com.korhan34.roomwithaview;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Word.class}, version = 1)
 public abstract class WordRoomDatabase extends RoomDatabase {
     private static volatile WordRoomDatabase INSTANCE;
+
+    private static RoomDatabase.Callback roomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase database) {
+            super.onOpen(database);
+            new PopulateDatabaseAsync(INSTANCE).execute();
+        }
+    }
+
+    private static class PopulateDatabaseAsync extends AsyncTask<Void, Void, Void> {
+        private final WordDao dao;
+
+        PopulateDatabaseAsync(WordRoomDatabase database) {
+            dao = database.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            dao.deleteAll();
+            dao.insert(new Word("Hello"));
+            dao.insert(new Word("World"));
+
+            return null;
+        }
+    }
 
     public abstract WordDao wordDao();
 
@@ -16,8 +44,9 @@ public abstract class WordRoomDatabase extends RoomDatabase {
             synchronized (WordRoomDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                    WordRoomDatabase.class, "word_database")
-                    .build();
+                            WordRoomDatabase.class, "word_database")
+                            .addCallback(roomDatabaseCallback)
+                            .build();
                 }
             }
         }
